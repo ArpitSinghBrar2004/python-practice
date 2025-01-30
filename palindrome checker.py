87da36e65dfaa9f8c584bfd6a2ff7c559ec05a56
Arpit Singh Brar <arpitsinghbrar853@gmail.com>
2025-01-30 23:19:14 +0530

    Add files via upload

---
+++ palindrome checker.py
@@ -0,0 +1,6 @@
+my_strig=input("enter String \n")
+reversed_string= my_strig[::-1]
+if(my_strig == reversed_string):
+    print("string is palandrome")
+else:
+    print("strinng is not palandrome")
