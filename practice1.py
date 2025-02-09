d202ee4c5f1d96397e56c79af384bd302c034bed
Arpit Singh Brar <arpitsinghbrar853@gmail.com>
2025-02-09 12:14:03 +0530

    Add files via upload

---
+++ practice1.py
@@ -0,0 +1,8 @@
+num1 = int(input("Enter 1st number: "))
+num2 = int(input("Enter 2nd number: "))
+
+def sum_of_two_num(num1, num2):
+    return num1 + num2
+
+sum = sum_of_two_num(num1, num2)
+print("The sum is:", sum)
