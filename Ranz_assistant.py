35e220df963f94b29251225ba75b3b6546ecc56d
Arpit Singh Brar <arpitsinghbrar853@gmail.com>
2025-07-21 11:57:49 +0530

    Add files via upload

--- Ranz_assistant.py
+++ Ranz_assistant.py
@@ -14,10 +14,15 @@ bot = commands.Bot(command_prefix="!",intents=intents)
 
 @bot.event
 async def on_ready():
+    await bot.tree.sync()
     print(f"{bot.user} is online !")
 @bot.event
 async def on_message(msg):
     if msg.author.id != bot.user.id:
         await msg.channel.send(f"Interesting message ,{msg.author.mention} ")
+@bot.tree.command(name="greet",description="sends a greeting to user ")
+async def greet (interaction: discord.Interaction):
+    username=interaction.user.mention
+    await interaction.response.send_message(f"Hello there ,{username}")
 
 bot.run(Token)
