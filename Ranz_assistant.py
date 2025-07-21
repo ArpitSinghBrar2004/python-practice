f81e0d4a959ab2b26fd9dd45a3234c4eaf93898c
Arpit Singh Brar <arpitsinghbrar853@gmail.com>
2025-07-21 11:16:33 +0530

    Add files via upload

---
+++ Ranz_assistant.py
@@ -0,0 +1,23 @@
+import os
+import discord
+from discord.ext import commands
+from discord import app_commands
+from dotenv import load_dotenv
+
+load_dotenv()
+Token = os.getenv("Discord_Token")
+
+intents= discord.Intents.default()
+intents.message_content=True
+
+bot = commands.Bot(command_prefix="!",intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"{bot.user} is online !")
+@bot.event
+async def on_message(msg):
+    if msg.author.id != bot.user.id:
+        await msg.channel.send(f"Interesting message ,{msg.author.mention} ")
+
+bot.run(Token)
